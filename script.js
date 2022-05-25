e0ed35c799ee76a6ba1ad43f6fab09491bdc874e
Gokulcasper <ronitcasper215@gmail.com>
2022-05-25 15:13:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert("Welcome To JS From External File");
+
+console.log("Welcome to JS");
+console.log(12345);
+console.log(12.34);
+console.log(true);
+console.log([10, 20, 30, 40, 50]);
+console.log({ Name: "Gokul", age: 25 });
+console.table({ Name: "Gokul", age: 25 });
+console.error("Sample Error");
+console.warn("Sample Error");
+console.time("Timer");
+for (i = 1; i < 10; i++) {
+  console.log(i);
+}
+console.timeEnd("Timer");
+console.clear();
